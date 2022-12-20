9173e942f1bbd51bcc299f0a85925518cd78a0ea
Tiaan Fairchild <tiaanbrunt@gmail.com>
2022-12-20 15:38:06 +1300

    Fix: removed output field in blog collection

--- cloudcannon.config.cjs
+++ cloudcannon.config.cjs
@@ -29,7 +29,7 @@ module.exports = {
         },
         blog: {
             path: "content/blog/",
-            'output': true,
+            // 'output': true,
             // url: '/blog/[slug]',
             _enabled_editors: [
                 "content",
